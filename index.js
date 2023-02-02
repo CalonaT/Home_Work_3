631ad7a73e029ec440d1f87a98b676b94950ca86
CalonaT <90524227+CalonaT@users.noreply.github.com>
2023-02-02 17:51:56 +0400

    resend hw3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const userNumber = +prompt("Введіть число , яке кратні 5");
 
 for (let i = 0; i <= userNumber; i++) {
+  if (userNumber < 5) {
+    alert("Sorry, no numbers");
+    break;
+  }
   if (i % 5 === 0) {
     console.log(i);
-  } else if (userNumber != 5) {
-    alert("Sorry, no numbers");
   }
 }
